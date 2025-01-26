1a5f271d603aa9cfe9372effd65bf74b81fceaad
aarush005 <astaarush005@gmail.com>
2025-01-26 12:51:07 +0530

    Feat: Add the drag functionality

--- src/Swimlane.js
+++ src/Swimlane.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import Card from './Card';
 import './Swimlane.css';
+import dragula from 'dragula';
 
 export default class Swimlane extends React.Component {
+  componentDidMount() {
+    const swimlanes = document.querySelectorAll('.Swimlane-column');
+    const drake = dragula(swimlanes, {
+      moves: true,
+      accepts: (el, target, source, sibling) => {
+        // Allow cards to be moved between swimlanes
+        return true;
+      },
+      revertOnSpill: true,
+    });
+
+    drake.on('drop', (el, target, source, sibling) => {
+      const swimlaneId = target.id;
+      const card = el;
+
+      // Update card color based on swimlane
+      if (swimlaneId === 'backlog-swimlane') {
+        card.classList.remove('in-progress', 'complete');
+        card.classList.add('backlog');
+      } else if (swimlaneId === 'in-progress-swimlane') {
+        card.classList.remove('backlog', 'complete');
+        card.classList.add('in-progress');
+      } else if (swimlaneId === 'complete-swimlane') {
+        card.classList.remove('backlog', 'in-progress');
+        card.classList.add('complete');
+      }
+    });
+  }
+
   render() {
     const cards = this.props.clients.map(client => {
       return (
@@ -16,12 +46,12 @@ export default class Swimlane extends React.Component {
       );
     })
     return (
-      <div className="Swimlane-column">
+      <div className="Swimlane-column" id={this.props.name}>
         <div className="Swimlane-title">{this.props.name}</div>
-        <div className="Swimlane-dragColumn" ref={this.props.dragulaRef}>
+        <div className="Swimlane-dragColumn">
           {cards}
         </div>
-      </div>);
+      </div>
+    );
   }
-
 }
